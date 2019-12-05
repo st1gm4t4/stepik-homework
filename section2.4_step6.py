c2a017d885c69d27451f9264732b16ee164cf1f5
unknown <tkilla.msk@gmail.com>
2019-12-05 23:10:55 +0300

    задания второго модуля

---
+++ section2.4_step6.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+import time
+
+browser = webdriver.Chrome()
+browser.implicitly_wait(5)
+browser.get("http://suninjuly.github.io/cats.html")
+
+button = browser.find_element_by_id("button")
+button.click()
---
+++ section2.4_step8.py
@@ -0,0 +1,35 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+import time
+import math
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+try:
+    browser = webdriver.Chrome()
+    browser.get("http://suninjuly.github.io/explicit_wait2.html")
+    price = "$100"
+    button = browser.find_element_by_id("book")
+    # говорим Selenium проверять в течение 5 секунд, пока кнопка не станет кликабельной
+
+
+    WebDriverWait(browser, 15).until(
+            EC.text_to_be_present_in_element((By.ID, "price"), price)
+        )
+    button.click()
+    x = browser.find_element_by_id("input_value").text
+    y = calc(x)
+    answer = browser.find_element_by_id("answer")
+    answer.send_keys(y)
+    button = browser.find_element_by_id("solve")
+    browser.execute_script("return arguments[0].scrollIntoView(true);", button)
+    button.click()
+
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
