e606ce9890b971f19e45df00e0c8591f0da303e8
unknown <tkilla.msk@gmail.com>
2019-12-05 23:20:12 +0300

    deleted corrupted files

--- section2.4_step6.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-import time
-
-browser = webdriver.Chrome()
-browser.implicitly_wait(5)
-browser.get("http://suninjuly.github.io/cats.html")
-
-button = browser.find_element_by_id("button")
-button.click()
